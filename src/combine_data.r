f4d36ab455415e5c744682c80e67b4e4d6ab972e
BrianTruong23 <truongthoithang@gmail.com>
2023-10-03 21:13:39 -0500

    Add dataset

---
+++ src/combine_data.r
@@ -0,0 +1,12 @@
+
+
+my_function <- function() { # create a function with the name my_function
+  print("Hello World!")
+}
+
+# Read a text file using read.delim()
+myData = read.delim("geeksforgeeks.txt", header = FALSE)
+print(myData)
+
+my_function
+
